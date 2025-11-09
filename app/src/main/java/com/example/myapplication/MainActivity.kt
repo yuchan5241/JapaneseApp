bbd152189de081a2d3900fe9b172387572d47b26
Yuchan <grigomo10@gmail.com>
2025-11-09 10:22:21 +0900

    Android Studio Compose Tutorial

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +27,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "such",
+                        name = "android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,16 +36,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-//    Text(
-//        text = "Goodbye World"
-//
-//    )
+    Column(
+       modifier = Modifier.background(Color.Black)
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = Color.Blue,
+            fontSize = 15.sp
+        )
+        Text(
+            text = "Hello Yuchan!",
+            color = Color.Blue,
+            fontSize = 15.sp
+        )
+    }
 
 }
 
