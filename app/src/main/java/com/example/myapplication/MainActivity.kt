1d815c7c0d66e569afdc883331d4c67f31b95e6a
Yuchan <grigomo10@gmail.com>
2025-11-09 10:26:12 +0900

    Android Studio Compose Tutorial

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,7 +43,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(
-       modifier = Modifier.background(Color.Black)
+       horizontalAlignment = Alignment.End,
+       verticalArrangement = Arrangement.Bottom
     ) {
         Text(
             text = "Hello $name!",
@@ -51,7 +54,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(
             text = "Hello Yuchan!",
             color = Color.Blue,
-            fontSize = 15.sp
+            fontSize = 10.sp
         )
     }
 
